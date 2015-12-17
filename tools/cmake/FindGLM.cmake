d4cf04700d4c942aa21dabe9dc17dd1ff85ffb6c
Johannes Wolf <git@sigabrt.de>
2015-12-17 02:36:33 +0100

    added cmake script to find glm

---
+++ tools/cmake/FindGLM.cmake
@@ -0,0 +1,17 @@
+# Find GLM
+#
+# This module defines:
+#     GLM_FOUND
+#     GLM_INCLUDE_DIR
+
+find_path(GLM_INCLUDE_DIR glm.hpp
+	HINTS ENV GLMDIR
+	PATH_SUFFIXES glm include/glm)
+
+include(FindPackageHandleStandardArgs)
+
+find_package_handle_standard_args(GLM DEFAULT_MSG
+	GLM_INCLUDE_DIR)
+
+mark_as_advanced(SDL2_INCLUDE_DIR)
+
