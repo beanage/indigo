fe281328d7f3f255ef7cb1e514c8728b6207a7eb
Joseph Birkner <birknerj@cip.ifi.lmu.de>
2016-01-01 17:37:25 +0100

    added debug.hpp

---
+++ include/debug.hpp
@@ -0,0 +1,25 @@
+#ifndef __INDIGO_DEBUG_HPP__
+#define __INDIGO_DEBUG_HPP__
+
+#include <iostream>
+#include "basic_geom.hpp"
+
+namespace indigo {
+    std::ostream& operator<<(std::ostream& out, const glm::vec3& vec) {
+        return out << "{x: " << vec.x << ", y: " << vec.y << ", z: " << vec.z << "}";
+    }
+
+    std::ostream& operator<<(std::ostream& out, const glm::quat& q) {
+        return out << "{x: " << q.x << ", y: " << q.y << ", z: " << q.z << ", w: " << q.w <<"}";
+    }
+
+    std::ostream& operator<<(std::ostream& out, const aabb& r) {
+        return out << "{size: " << r.size << ", pos: " << r.front_bottom_left << "}";
+    }
+
+    std::ostream& operator<<(std::ostream& out, const box& r) {
+        return out << "{size: " << r.size << "}";
+    }
+}
+
+#endif
