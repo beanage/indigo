58e3019a4c0ebd314229e21523ace27ad9db24a6
Johannes Wolf <git@sigabrt.de>
2016-01-10 03:04:33 +0100

    Added switch_t class. It allows switch/case statements using operator==. E.g. for use with std::string.

---
+++ include/switch_t.hpp
@@ -0,0 +1,50 @@
+#ifndef __SWITCH_T_HPP_INCLUDED__
+#define __SWITCH_T_HPP_INCLUDED__
+
+namespace indigo
+{
+// usage:
+//     std::string input = ... ;
+//
+//     switch_t(input)
+//         .case_t("A", [&](){
+//             printf("A selected\n");
+//         })
+//         .case_t("B", ...
+//             ...
+//         )
+//         .default_t(...
+//             ...
+//         );
+//
+template <typename T>
+struct switch_t
+{
+    switch_t(T const& v) : v_(v), break_(false)
+    {}
+
+    template <typename F>
+    switch_t<T>& case_t(T const& c, F f)
+    {
+        if (!break_ && v_ == c) {
+            f();
+            break_ = true;
+        }
+
+        return *this;
+    }
+
+    template <typename F>
+    void default_t(F f)
+    {
+        if (!break_)
+            f();
+    }
+
+private:
+    T const& v_;
+    bool break_;
+};
+}
+
+#endif
