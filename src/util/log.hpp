9829fbb524cf0b657421ada8727ad839a40bb97d
Johannes Wolf <git@sigabrt.de>
2016-04-09 22:24:44 +0200

    Implemented basic logging function

--- src/util/log.hpp
+++ src/util/log.hpp
@@ -4,16 +4,34 @@
 #include <fstream>
 #include <iostream>
 
-namespace indigo {
+namespace indigo
+{
+namespace log
+{
 static const std::string default_log = "default.log";
 
-void log(const std::string& msg)
+template <typename T>
+void write_impl(T&& v)
 {
     static std::ofstream log_file;
     if (!log_file.is_open())
         log_file.open(default_log);
 
-    log_file << msg << std::endl;
-    std::cout << msg << std::endl;
+    log_file << v;
+    std::cout << v;
+}
+
+template <typename T, typename ...Tail>
+void write_impl(T&& v, Tail&&... tail)
+{
+	write_impl(v);
+	write_impl(tail...);
+}
+
+template <typename ...Args>
+void write(Args&&... args)
+{
+	write_impl(args..., "\n");
+}
 }
 }
